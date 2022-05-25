d2ab1ea93cb44e6a643604c6905905d48448ce02
Abdellatif-ait <ja_aiteur@esi.dz>
2022-05-25 09:48:02 +0100

    initialzing prisma and typescript

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
 });
 
-app.use('/api', require('./routes/api.route'));
-
 app.use((req, res, next) => {
   next(createError.NotFound());
 });
