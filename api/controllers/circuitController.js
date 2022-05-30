da66b458252776ce7a9926de8d0e30345def2007
Abdellatif-ait <ja_aiteur@esi.dz>
2022-05-30 23:18:19 +0100

    i separated all the routes and add new route for circuit suggestion

---
+++ api/controllers/circuitController.js
@@ -0,0 +1,56 @@
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function getHandler(_req,res){
+    const circuits= await prisma.circuit.findMany()
+    res.status(200).json({status:200,data:circuits,message:"here's what we found"})
+}
+async function getUserHandler(req,res){
+    //const {id}=req.tourist
+    const id=req.body.id
+    try {
+        const circuits=await prisma.circuit.findMany({where:{
+            touristid:id
+        }})
+        res.status(200).json({status:200,data:circuits,message:"here's what we found"})
+    } catch (error) {
+        res.status(500).json({ status: 500, message: "something went wrong! try later" })
+    }
+}
+async function postHandler(req,res){
+    //const {id}=req.tourist
+    const {id,content}=req.body
+    try {
+        const circuit=await prisma.circuit.create({
+            data:{
+                touristid:id
+            }
+        })
+        for(let x of content){
+            await prisma.circuitContent.create({
+                data:{
+                    circuitId:circuit.id,
+                    lieuId:x.id,
+                    position:x.position
+                }
+            })
+        }
+        res.status(201).josn({status:201,data:circuit,message:"added successfully"})
+    } catch (error) {
+        res.status(500).json({ status: 500, message: "something went wrong! try later" })
+    }
+}
+
+async function deleteHandler(req,res){
+    const{id}=req.params
+    try {
+        const circuit=await prisma.circuit.delete({where:{
+            id:id
+        }})
+        res.status(201).json({status:201,data:circuit,message:"deleted successfully"})
+    } catch (error) {
+        res.status(500).json({ status: 500, message: "something went wrong! try later" })      
+    }
+}
+
+module.exports={getHandler,getUserHandler,postHandler,deleteHandler}
--- api/controllers/touristController.js
+++ api/controllers/touristController.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 const bcrypt = require('bcrypt')
 const generateToken = require('../utils/generateToken')
-const prisma = new PrismaClient()
 
 async function getHandler(_req, res) {
     const tourist = await prisma.tourist.findMany();
@@ -84,57 +84,18 @@ async function followHandler(req, res) {
         res.status(500).json({ status: 500, message: "something went wrong! try lated" })
     }
 }
-async function postHandler(req, res) {
+async function deleteFollowHandler(req,res){
     //const {id}=req.tourist
-    const { id, description } = req.body
+    const {idAbonné,idAbonnement}=req.body
     try {
-        const post = await prisma.post.create({
-            data: {
-                description: description,
-                touristid: id
-            }
-        })
-        res.status(200).json({ status: 201, data: post, message: "posted successfuly" })
+        const follow=await prisma.abonne.delete({where:{
+            aboneeid:idAbonné,
+            abonnementsid:idAbonnement
+        }})
+        res.status(200).json({status:200,data:follow,message:"deleted successfully"})
     } catch (error) {
-        console.log(error.message)
-
-        res.status(500).json({ status: 500, message: "something went wrong! try later" })
-    }
-}
-async function visiteHandler(req, res) {
-    //const {id}=req.tourist
-    const { id, idLieu } = req.body
-    try {
-        const visite = await prisma.visiter.create({
-            data: {
-                visiteurid: id,
-                placeid: idLieu
-            }
-        })
-        res.status(201).json({ status: 201, data: visite, message: "visite has been added successfuly" })
-    } catch (error) {
-        console.log(error.message)
-
-        res.status(500).json({ status: 500, message: "something went wrong! try later" })
-
-    }
-}
-async function visiteDeleteHandler(req, res) {
-    //const {id}=req.tourist
-    const { id, idLieu } = req.body
-    try {
-        const visite =await prisma.visiter.delete({
-            where: {
-                visiteurid: id,
-                placeid: idLieu
-            }
-        })
-        res.status(200).json({ status: 200, message: "deleted successfuly", data: visite })
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({ status: 500, message: "something went wrong! try later" })
-
+        res.status(500).json({ status: 500, message: "something went wrong! try lated" })
     }
 }
 
-module.exports = { getHandler, getByIdHandler, registerHandler, loginHandler, followHandler, postHandler, visiteHandler, visiteDeleteHandler }
+module.exports = { getHandler, getByIdHandler, registerHandler, loginHandler, followHandler, deleteFollowHandler }
