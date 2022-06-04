95e134620ae93093c6186e35fcd76a312a52e4b1
Abdellatif-ait <ja_aiteur@esi.dz>
2022-06-04 17:55:21 +0100

    fixing the migration problem

--- api/controllers/eventController.js
+++ api/controllers/eventController.js
@@ -1,61 +1,63 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-async function getHandler(_req,res){
+async function getHandler(_req, res) {
     try {
         const events = await prisma.event.findMany()
-        res.status(200).json({status:200,data:events,message:"Here's what we found"})
+        res.status(200).json({ status: 200, data: events, message: "Here's what we found" })
     } catch (error) {
         res.status(500).json({ status: 500, message: "something went wrong! try later" })
     }
 }
-async function getByIdHandler(req,res){
-    const id= req.params.id;
+async function getByIdHandler(req, res) {
+    const id = req.params.id;
     try {
-        const events= await prisma.event.findMany({
-            where:{
-                lieuId:id
+        const events = await prisma.event.findMany({
+            where: {
+                lieuId: id
             }
         })
-        res.status(200).json({status:200,data:events,message:"here's what we found"})
+        res.status(200).json({ status: 200, data: events, message: "here's what we found" })
     } catch (error) {
         res.status(500).json({ status: 500, message: "something went wrong! try later" })
     }
 }
-async function postHandler(req,res){
-    const {title,date,id}=req.body;
+async function postHandler(req, res) {
+    const { title, date, id } = req.body;
     try {
-        const event= await prisma.event.findFirst({where:{
-            title:title,
-            date:date
-        }})
-        if(!event){
-            const newEvent= await prisma.event.create({
-                data:{
-                    title:title,
-                    date:date,
-                    lieuId:id
+        const event = await prisma.event.findFirst({
+            where: {
+                title: title,
+                date: date
+            }
+        })
+        if (!event) {
+            const newEvent = await prisma.event.create({
+                data: {
+                    title: title,
+                    date: date,
+                    lieuId: id
                 }
             })
-            res.status(201).json({status:201,data:newEvent,message:"event created succcessfuly"})
+            res.status(201).json({ status: 201, data: newEvent, message: "event created succcessfuly" })
         }
-        res.status(400).json({status:400,message:"already exist"})
+        res.status(400).json({ status: 400, message: "already exist" })
     } catch (error) {
         res.status(500).json({ status: 500, message: "something went wrong! try later" })
     }
 }
-async function deleteHandler(req,res){
-    const id= req.params.id;
+async function deleteHandler(req, res) {
+    const id = req.params.id;
     try {
-        const event= await prisma.event.delete({
-            where:{
-                id:id
+        const event = await prisma.event.delete({
+            where: {
+                id: id
             }
         })
-        res.status(201).json({status:201,data:event,message:"Deleted successfuly"})
+        res.status(201).json({ status: 201, data: event, message: "Deleted successfuly" })
     } catch (error) {
         res.status(500).json({ status: 500, message: "something went wrong! try later" })
     }
 }
 
-module.exports={getHandler,getByIdHandler,postHandler,deleteHandler}
+module.exports = { getHandler, getByIdHandler, postHandler, deleteHandler }
