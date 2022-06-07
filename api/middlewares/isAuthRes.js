c2f60212d105875c230c5aef128b539e8d307aea
Abdellatif-ait <ja_aiteur@esi.dz>
2022-06-07 20:07:51 +0100

    Fixing auth

--- api/middlewares/isAuthRes.js
+++ api/middlewares/isAuthRes.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('@prisma/client')
 const jwt = require('jsonwebtoken')
 const prisma = new PrismaClient()
 const isAuthRes = async (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.headers.token;
     try {
         const responsable = jwt.verify(token, process.env.JWT_SECRET)
         req.res = responsable
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const createError = require('http-errors');
 const morgan = require('morgan');
 const cors = require('cors')
+const cookieParser= require('cookie-parser')
 require('dotenv').config();
 const touristRoute = require('./api/routes/tourist')
 const annonceRoute = require('./api/routes/annonce')
@@ -17,6 +18,7 @@ const eventRoute=require('./api/routes/event')
 /* MIDDLEWARES */
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cors({
